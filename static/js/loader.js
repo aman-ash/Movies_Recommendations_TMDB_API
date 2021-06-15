492f220f63e0dca4f711e77a491a443c9e4e6585
amanash <amanmahore.ash@gmail.com>
2021-06-15 09:35:08 +0530

    Movies

--- static/js/loader.js
+++ static/js/loader.js
@@ -70,3 +70,75 @@ function openNavi() {
   function closeNavf() {
     document.getElementById("myf").style.width = "0%";
   }
+
+  //9th movie
+  function openNavg() {
+    document.getElementById("myg").style.width = "100%";
+  }
+  
+  function closeNavg() {
+    document.getElementById("myg").style.width = "0%";
+  }
+
+  //10th movie
+  function openNavh() {
+    document.getElementById("myh").style.width = "100%";
+  }
+  
+  function closeNavh() {
+    document.getElementById("myh").style.width = "0%";
+  }
+
+  //11th movie
+  function openNavhi() {
+    document.getElementById("myhi").style.width = "100%";
+  }
+  
+  function closeNavhi() {
+    document.getElementById("myhi").style.width = "0%";
+  }
+
+  //12th movie
+  function openNavj() {
+    document.getElementById("myj").style.width = "100%";
+  }
+  
+  function closeNavj() {
+    document.getElementById("myj").style.width = "0%";
+  }
+
+  //13th movie
+  function openNavk() {
+    document.getElementById("myk").style.width = "100%";
+  }
+  
+  function closeNavk() {
+    document.getElementById("myk").style.width = "0%";
+  }
+
+  //14th movie
+  function openNavl() {
+    document.getElementById("myl").style.width = "100%";
+  }
+  
+  function closeNavl() {
+    document.getElementById("myl").style.width = "0%";
+  }
+
+  //15th movie
+  function openNavm() {
+    document.getElementById("mym").style.width = "100%";
+  }
+  
+  function closeNavm() {
+    document.getElementById("mym").style.width = "0%";
+  }
+
+  //16th movie
+  function openNavn() {
+    document.getElementById("myn").style.width = "100%";
+  }
+  
+  function closeNavn() {
+    document.getElementById("myn").style.width = "0%";
+  }
